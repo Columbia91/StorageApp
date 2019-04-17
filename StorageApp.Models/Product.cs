2a214351cdb47e726b029194583d8d54912a4a0a
cougar91 <alpha.z91@mail.ru>
2019-04-17 16:07:41 +0600

    Добавьте файлы проекта.

---
+++ StorageApp.Models/Product.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StorageApp.Models
+{
+    public class Product : Entity
+    {
+        public string Name { get; set; }
+        public string Manufacturer { get; set; }
+        public double Price { get; set; }
+        public int Quantity { get; set; }
+
+        public void Show()
+        {
+            Console.WriteLine("Данные продукта\n" +
+                "Название: {0}\n" +
+                "Производитель: {1}\n" +
+                "Цена: {2}\n" +
+                "Количество: {3}",
+                Name, Manufacturer, Price, Quantity);
+        }
+    }
+}
---
+++ StorageApp.Models/ProductsContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StorageApp.Models
+{
+    public class ProductsContext : DbContext
+    {
+        public ProductsContext() : base ("appConnection")
+        {}
+        
+        public DbSet<Product> Products { get; set; }
+    }
+}
